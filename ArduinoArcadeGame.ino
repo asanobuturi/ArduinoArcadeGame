24c9eb2fbdc29dcfa46008b8ffcc6204a1546dd3
DIV-IJ <s2019062@asano.ed.jp>
2021-12-10 20:14:48 +0900

    Loaded misakiUTF16 & showed 「ｱｻﾉﾌﾞﾂﾘ 浅野物理」

--- ArduinoArcadeGame.ino
+++ ArduinoArcadeGame.ino
@@ -1,97 +1,74 @@
-// testshapes demo for Adafruit RGBmatrixPanel library.
-// Demonstrates the drawing abilities of the RGBmatrixPanel library.
-// For 16x32 RGB LED matrix:
-// http://www.adafruit.com/products/420
-
-// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
-// for Adafruit Industries.
-// BSD license, all text above must be included in any redistribution.
-
 #include <RGBmatrixPanel.h>
+#include <misakiUTF16.h>
 
-// Most of the signal pins are configurable, but the CLK pin has some
-// special constraints.  On 8-bit AVR boards it must be on PORTB...
-// Pin 8 works on the Arduino Uno & compatibles (e.g. Adafruit Metro),
-// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
-// on the same PORT as the RGB data pins (D2-D7)...
-// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
-// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
-// Matrix Shield, cut trace between CLK pads and run a wire to A4).
-
-#define CLK  8   // USE THIS ON ARDUINO UNO, ADAFRUIT METRO M0, etc.
-//#define CLK A4 // USE THIS ON METRO M4 (not M0)
-//#define CLK 11 // USE THIS ON ARDUINO MEGA
+//ピンの諸設定
+#define CLK  8
 #define OE   9
 #define LAT 10
 #define A   A0
 #define B   A1
 #define C   A2
 
+//まぶしすぎるので少し暗めのRGB
+#define RGB4(i,j,k)  matrix.Color333(i,j,k)
+
+//色の諸設定
+#define BLACK   RGB4(0,0,0)
+#define BLUE    RGB4(0,0,4)
+#define RED     RGB4(4,0,0)
+#define GREEN   RGB4(0,4,0)
+#define CYAN    RGB4(0,4,4)
+#define MAGENTA RGB4(4,0,4)
+#define YELLOW  RGB4(4,4,0)
+#define WHITE   RGB4(4,4,4)
+
+//LEDマトリクスの初期化
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+//半角文字を描く
+void drawMisakiHankaku ( int16_t x,int16_t y,char * text,uint16_t color )  {
+  int n=0;
+  byte buf[20][8];
+
+  while(*text)
+    text = getFontData(&buf[n++][0], text);
+    
+  for (byte i=0; i < 8; i++) {
+    for (byte j=0; j < n; j++){
+      for (byte k=0; k<4;k++){
+        matrix.drawPixel(x + j * 4 + k, y + i, bitRead(buf[j][i],7-k) ? color: BLACK);
+        }
+    }
+  }
+ }
+ 
+//全角文字を描く
+void drawMisakiZenkaku ( int16_t x,int16_t y,char * text,uint16_t color )  {
+  int n=0;
+  byte buf[20][8];
+
+  while(*text)
+    text = getFontData(&buf[n++][0], text);
+    
+  for (byte i=0; i < 8; i++) {
+    for (byte j=0; j < n; j++){
+      for (byte k=0; k<8;k++){
+        matrix.drawPixel(x + j * 8 + k, y + i, bitRead(buf[j][i],7-k) ? color: BLACK);
+        }
+    }
+  }
+ }
+
 void setup() {
-
-  matrix.begin();
-
-  // draw a pixel in white
-  matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7));
-  delay(500);
-
-  // fix the screen with green
-  matrix.fillRect(0, 0, 32, 16, matrix.Color333(0, 7, 0));
-  delay(500);
-
-  // draw a box in yellow
-  matrix.drawRect(0, 0, 32, 16, matrix.Color333(7, 7, 0));
-  delay(500);
-
-  // draw an 'X' in red
-  matrix.drawLine(0, 0, 31, 15, matrix.Color333(7, 0, 0));
-  matrix.drawLine(31, 0, 0, 15, matrix.Color333(7, 0, 0));
-  delay(500);
-
-  // draw a blue circle
-  matrix.drawCircle(7, 7, 7, matrix.Color333(0, 0, 7));
-  delay(500);
-
-  // fill a violet circle
-  matrix.fillCircle(23, 7, 7, matrix.Color333(7, 0, 7));
-  delay(500);
-
-  // fill the screen with black
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
-
-  // draw some text!
-  matrix.setCursor(1, 0);  // start at top left, with one pixel of spacing
-  matrix.setTextSize(1);   // size 1 == 8 pixels high
-
-  // print each letter with a rainbow color
-  matrix.setTextColor(matrix.Color333(7,0,0));
-  matrix.print('1');
-  matrix.setTextColor(matrix.Color333(7,4,0));
-  matrix.print('6');
-  matrix.setTextColor(matrix.Color333(7,7,0));
-  matrix.print('x');
-  matrix.setTextColor(matrix.Color333(4,7,0));
-  matrix.print('3');
-  matrix.setTextColor(matrix.Color333(0,7,0));
-  matrix.print('2');
-
-  matrix.setCursor(1, 9);  // next line
-  matrix.setTextColor(matrix.Color333(0,7,7));
-  matrix.print('*');
-  matrix.setTextColor(matrix.Color333(0,4,7));
-  matrix.print('R');
-  matrix.setTextColor(matrix.Color333(0,0,7));
-  matrix.print('G');
-  matrix.setTextColor(matrix.Color333(4,0,7));
-  matrix.print('B');
-  matrix.setTextColor(matrix.Color333(7,0,4));
-  matrix.print('*');
-
-  // whew!
+  matrix.begin();//マトリクスの開始
+  matrix.setRotation(1);//マトリクスパネルの回転を設定
+  matrix.fillScreen(BLACK);//黒で埋める
+  drawMisakiHankaku(0,0,"ｱｻﾉ",RED);//ｱｻﾉ
+  drawMisakiHankaku(0,8,"ﾌﾞﾂﾘ",GREEN);//ﾌﾞﾂﾘ
+  drawMisakiZenkaku(0,16,"浅野",YELLOW);//浅野
+  drawMisakiZenkaku(0,24,"物理",MAGENTA);//物理
 }
 
 void loop() {
-  // Do nothing -- image doesn't change
+  
 }
