fb5e24bc665d2aea14d292229418338397a3120e
DIV-IJ <s2019062@asano.ed.jp>
2021-12-29 21:30:17 +0900

    ☑added ArduinoArcadeGame.h

---
+++ ArduinoArcadeGame.h
@@ -0,0 +1,172 @@
+/*
+ArduinoArcadeGame.h
+独自の関数一覧
+  void initialize();
+    初期化
+  void sound(uint16_t note);
+    音を鳴らす
+  void drawMisakiZenkaku ( int16_t x,int16_t y,char * text,uint16_t color );
+    全角文字を描く
+  void drawMisakiHankaku ( int16_t x,int16_t y,char * text,uint16_t color );
+    半角文字を描く
+  void scrollMisakiZenkaku ( int16_t x,int16_t y,int16_t w,char * text,uint16_t color,int16_t frame );
+    全角文字のスクロール
+  void scrollMisakiHankaku ( int16_t x,int16_t y,int16_t w,char * text,uint16_t color,int16_t frame );
+    半角文字のスクロール
+  bool bPressed(int16_t num)
+    ボタンが押されているかどうか確認する
+
+  RGB4(r,g,b)
+    matrix.color333と同じ
+    RGBをそれぞれ0~3(本来は0~7だがまぶしすぎるため)で調整する
+
+matrix.[Adafruit GFX Libraryの関数]
+  Adafruit GFX Libraryの関数を実行
+*/
+
+#include <RGBmatrixPanel.h>
+#include <misakiUTF16.h>
+#include "pitches.h"
+
+//LEDマトリクスのピンの諸設定
+#define CLK  8
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+
+//まぶしすぎるので少し暗めのRGB
+#define RGB4(r,g,b)  matrix.Color333(r,g,b)
+
+//色の諸設定
+#define BLACK   RGB4(0,0,0)
+#define BLUE    RGB4(0,0,3)
+#define RED     RGB4(3,0,0)
+#define GREEN   RGB4(0,3,0)
+#define CYAN    RGB4(0,3,3)
+#define MAGENTA RGB4(3,0,3)
+#define YELLOW  RGB4(3,3,0)
+#define WHITE   RGB4(3,3,3)
+
+//74HC165のピンの設定
+#define SL    A3//SH/LD
+#define CLKB  A4//CLK
+#define SER   A5//QH
+
+//パッシブブザーのピン
+#define BUZ 12
+
+
+//フレーム
+#define FPS 18
+
+//LEDマトリクスの初期化
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+
+int16_t n=0;
+byte buf[20][8];
+
+//全角文字を描く
+void drawMisakiZenkaku ( int16_t x,int16_t y,char * text,uint16_t color )  {
+  n=0;
+
+  while(*text)
+    text = getFontData(&buf[n++][0], text);
+    
+  for (byte i=0; i < 8; i++) {
+    for (byte j=0; j < n; j++){
+      for (byte k=0; k<8;k++){
+        if(bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 8 + k, y + i, color);
+        }
+    }
+  }
+ }
+
+//半角文字を描く
+void drawMisakiHankaku ( int16_t x,int16_t y,char * text,uint16_t color )  {
+  n=0;
+
+  while(*text)
+    text = getFontData(&buf[n++][0], text);
+    
+  for (byte i=0; i < 8; i++) {
+    for (byte j=0; j < n; j++){
+      for (byte k=0; k<4;k++){
+        if(bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 4 + k, y + i,color);
+        }
+    }
+  }
+ }
+ 
+ //全角文字のスクロール
+ void scrollMisakiZenkaku ( int16_t x,int16_t y,int16_t w,char * text,uint16_t color,int16_t frame ){
+  n=0;
+
+  while(*text)
+    text = getFontData(&buf[n++][0], text);
+
+  int16_t scrollLoop = frame % (8 * (n + 2));
+
+
+  for (byte i=0; i < 8; i++) {
+    for (byte j=0; j < n; j++){
+      for (byte k=0; k<8;k++){
+        if(x <= x + j * 8 + k + w - scrollLoop && x + j * 8 + k + w - scrollLoop < x + w && bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 8 + k + w - scrollLoop, y + i, color);
+        }
+    }
+  }
+  } 
+ 
+ //半角文字のスクロール
+ void scrollMisakiHankaku ( int16_t x,int16_t y,int16_t w,char * text,uint16_t color,int16_t frame ){
+  n=0;
+
+  while(*text)
+    text = getFontData(&buf[n++][0], text);
+
+  int16_t scrollLoop = frame % (4 * (n + 4));
+
+
+  for (byte i=0; i < 8; i++) {
+    for (byte j=0; j < n; j++){
+      for (byte k=0; k<4;k++){
+        if(x <= x + j * 4 + k + w - scrollLoop && x + j * 4 + k + w - scrollLoop < x + w && bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 4 + k + w - scrollLoop, y + i, color);
+        }
+    }
+  }
+  }
+
+//ボタンが押されているかどうか確認する
+//0 <= num <= 7
+byte buttonData;
+bool bPressed(int16_t num){
+    digitalWrite(SL,LOW);
+    digitalWrite(SL,HIGH);
+    buttonData = digitalRead(SER);//Hを読む
+    for(byte i=1; i<8; i++){
+      digitalWrite(CLKB, HIGH);
+      buttonData = buttonData << 1 | (digitalRead(SER));//G~Aを読む
+      digitalWrite(CLKB, LOW);
+      }
+    return !bitRead(buttonData,num);
+  }
+
+//音を鳴らす
+void sound(uint16_t note){
+  tone(BUZ,note,1200 / FPS);
+  }
+
+//初期化
+void initialize(){
+  matrix.begin();//マトリクスの開始
+  //74HC165のピンの設定
+  pinMode(CLKB,OUTPUT);
+  pinMode(SL,OUTPUT);
+  pinMode(SER,INPUT);
+  digitalWrite(SL,HIGH);
+  digitalWrite(CLKB,LOW);
+  pinMode(BUZ,OUTPUT);//スピーカーのピン
+  matrix.setRotation(1);//マトリクスパネルの回転を設定
+  matrix.fillScreen(BLACK);//黒で埋める
+  }
--- ArduinoArcadeGame.ino
+++ ArduinoArcadeGame.ino
@@ -1,147 +1,8 @@
-#include <RGBmatrixPanel.h>
-#include <misakiUTF16.h>
-#include "pitches.h"
+#include "ArduinoArcadeGame.h"
 
-//LEDマトリクスのピンの諸設定
-#define CLK  8
-#define OE   9
-#define LAT 10
-#define A   A0
-#define B   A1
-#define C   A2
-
-//まぶしすぎるので少し暗めのRGB
-#define RGB4(i,j,k)  matrix.Color333(i,j,k)
-
-//色の諸設定
-#define BLACK   RGB4(0,0,0)
-#define BLUE    RGB4(0,0,4)
-#define RED     RGB4(4,0,0)
-#define GREEN   RGB4(0,4,0)
-#define CYAN    RGB4(0,4,4)
-#define MAGENTA RGB4(4,0,4)
-#define YELLOW  RGB4(4,4,0)
-#define WHITE   RGB4(4,4,4)
-
-//74HC165のピンの設定
-#define SL    A3//SH/LD
-#define CLKB  A4//CLK
-#define SER   A5//QH
-
-//パッシブブザーのピン
-#define BUZ 12
-
-//ボタンが点灯しているかどうか
-#define bPressed(i) !bitRead(buttonData,i)
-
-//フレーム
 int16_t frame = 0;
-#define FPS 18
-
-//LEDマトリクスの初期化
-RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
-
-int16_t n=0;
-byte buf[20][8];
-
-//全角文字を描く
-void drawMisakiZenkaku ( int16_t x,int16_t y,char * text,uint16_t color )  {
-  n=0;
-
-  while(*text)
-    text = getFontData(&buf[n++][0], text);
-    
-  for (byte i=0; i < 8; i++) {
-    for (byte j=0; j < n; j++){
-      for (byte k=0; k<8;k++){
-        if(bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 8 + k, y + i, color);
-        }
-    }
-  }
- }
-
-//半角文字を描く
-void drawMisakiHankaku ( int16_t x,int16_t y,char * text,uint16_t color )  {
-  n=0;
-
-  while(*text)
-    text = getFontData(&buf[n++][0], text);
-    
-  for (byte i=0; i < 8; i++) {
-    for (byte j=0; j < n; j++){
-      for (byte k=0; k<4;k++){
-        if(bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 4 + k, y + i,color);
-        }
-    }
-  }
- }
- 
- //全角文字のスクロール(変数frameに応じてスクロール)
- void scrollMisakiZenkaku ( int16_t x,int16_t y,int16_t w,char * text,uint16_t color ){
-  n=0;
-
-  while(*text)
-    text = getFontData(&buf[n++][0], text);
-
-  int16_t loop = frame % (8 * (n + 2));
-
-
-  for (byte i=0; i < 8; i++) {
-    for (byte j=0; j < n; j++){
-      for (byte k=0; k<8;k++){
-        if(x <= x + j * 8 + k + w - loop && x + j * 8 + k + w - loop < x + w && bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 8 + k + w - loop, y + i, color);
-        }
-    }
-  }
-  } 
- 
- //半角文字のスクロール(変数frameに応じてスクロール)
- void scrollMisakiHankaku ( int16_t x,int16_t y,int16_t w,char * text,uint16_t color ){
-  n=0;
-
-  while(*text)
-    text = getFontData(&buf[n++][0], text);
-
-  int16_t loop = frame % (4 * (n + 4));
-
-
-  for (byte i=0; i < 8; i++) {
-    for (byte j=0; j < n; j++){
-      for (byte k=0; k<4;k++){
-        if(x <= x + j * 4 + k + w - loop && x + j * 4 + k + w - loop < x + w && bitRead(buf[j][i],7-k))matrix.drawPixel(x + j * 4 + k + w - loop, y + i, color);
-        }
-    }
-  }
-  }
-
-byte buttonData;
-//ボタンが押されているかどうか確認する
-void updateButton(){
-    digitalWrite(SL,LOW);
-    digitalWrite(SL,HIGH);
-    buttonData = digitalRead(SER);//Hを読む
-    for(byte i=1; i<8; i++){
-      digitalWrite(CLKB, HIGH);
-      buttonData = buttonData << 1 | (digitalRead(SER));//G~Aを読む
-      digitalWrite(CLKB, LOW);
-      }
-  }
-//ボタン番号
-int16_t bNum(int16_t n){
-    return 8 - n;
-  }
-
 void setup() {
-  matrix.begin();//マトリクスの開始
-  //74HC165のピンの設定
-  pinMode(CLKB,OUTPUT);
-  pinMode(SL,OUTPUT);
-  pinMode(SER,INPUT);
-  digitalWrite(SL,HIGH);
-  digitalWrite(CLKB,LOW);
-  pinMode(BUZ,OUTPUT);//スピーカーのピン
-  matrix.setRotation(1);//マトリクスパネルの回転を設定
-  matrix.fillScreen(BLACK);//黒で埋める
+  initialize();
 }
 
 void loop() {
@@ -149,30 +10,29 @@ void loop() {
   while(1){
     frame++;//フレームを1つ増やす
     matrix.fillScreen(BLACK);//フレームを黒に塗る
-    updateButton();
     if(bPressed(0)){
       matrix.fillCircle(7,9,2,RED);
-      tone(BUZ,NOTE_C6,1200 / FPS);
+      sound(NOTE_C6);
       }
     if(bPressed(1)){
       matrix.fillCircle(2,14,2,RED);
-      tone(BUZ,NOTE_D6,1200 / FPS);
+      sound(NOTE_D6);
       }
     if(bPressed(2)){
       matrix.fillCircle(7,14,2,YELLOW);
-      tone(BUZ,NOTE_E6,1200 / FPS);
+      sound(NOTE_E6);
       }
     if(bPressed(3)){
       matrix.fillCircle(12,14,2,RED);
-      tone(BUZ,NOTE_F6,1200 / FPS);
+      sound(NOTE_F6);
       }
     if(bPressed(4)){
       matrix.fillCircle(7,19,2,RED);
-      tone(BUZ,NOTE_G6,1200 / FPS);
+      sound(NOTE_G6);
       }
     if(bPressed(5)){
       matrix.fillCircle(14,7,1,MAGENTA);
-      tone(BUZ,NOTE_A6,1200 / FPS);
+      sound(NOTE_A6);
       }
     delay(1000 / FPS);//フレームを変えるまで待つ
   }
